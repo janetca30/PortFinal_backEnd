0039b76d62a056af583e7e5d4389132afeab9317
janetca30 <janetca30@gmail.com>
2023-03-26 17:47:46 -0300

    arreglos

--- src/main/java/com/janetca/portfinal/Security/Controller/AuthController.java
+++ src/main/java/com/janetca/portfinal/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portfinal-5ec3b.web.app"})
+@CrossOrigin
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
