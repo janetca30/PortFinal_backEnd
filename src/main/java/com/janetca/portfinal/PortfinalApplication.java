b55712cc163ee68a588c21b3ecf9647c01daf86f
janetca30 <janetca30@gmail.com>
2023-03-26 19:00:27 -0300

    controller

--- src/main/java/com/janetca/portfinal/PortfinalApplication.java
+++ src/main/java/com/janetca/portfinal/PortfinalApplication.java
@@ -18,7 +18,7 @@ public class PortfinalApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://portfinal-5ec3b.web.app").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
