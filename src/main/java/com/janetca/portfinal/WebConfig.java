98a4b9b2cf60641df0ea0970937cac4df8a273f1
janetca30 <janetca30@gmail.com>
2023-03-26 21:19:24 -0300

    webconfig

--- src/main/java/com/janetca/portfinal/WebConfig.java
+++ src/main/java/com/janetca/portfinal/WebConfig.java
@@ -7,11 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
+
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
-        
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("https://portfinal-5ec3b.web.app/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600);
+
     }
-    
+
 }
